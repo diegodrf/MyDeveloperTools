82fa3038716a76c94108120a206051767d391157
Diego Faria <diego.rdfaria@gmail.com>
2023-08-19 02:07:07 -0400

    Constant for routes

---
+++ MyDeveloperTools.App/Constants/RouteConstant.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace MyDeveloperTools.App.Constants
+{
+    public static class RouteConstant
+    {
+        public const string Base64Converter = "/tools/base64-converter";
+        
+        public static string GetRelativePath(this string path) => path.TrimStart('/');
+
+        public static IEnumerable<string> GetAllRoutes()
+        {
+            return typeof(RouteConstant)
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(x => x.IsLiteral && !x.IsInitOnly && x.FieldType == typeof(string))
+                .Select(x => x.GetValue(null)!.ToString()!)
+                .ToList();
+        }
+    }
+}
