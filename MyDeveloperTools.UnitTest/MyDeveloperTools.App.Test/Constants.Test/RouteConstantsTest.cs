9b1ff83d0a6029183046eea18811ce56c5e3204a
Diego Faria <diego.rdfaria@gmail.com>
2023-08-19 02:07:24 -0400

    unit test for constant routes

---
+++ MyDeveloperTools.UnitTest/MyDeveloperTools.App.Test/Constants.Test/RouteConstantsTest.cs
@@ -0,0 +1,29 @@
+﻿using MyDeveloperTools.App.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDeveloperTools.UnitTest.MyDeveloperTools.App.Test.Constants.Test
+{
+    public class RouteConstantsTest
+    {
+        [Fact]
+        public void WhenCallMethodToListRoutes_ShouldReturnAListOfRoutes()
+        {
+            var routes = RouteConstant.GetAllRoutes();
+
+            Assert.NotEmpty(routes);
+        }
+
+        [Fact]
+        public void WhenCallMethodToGetRelativePath_ShouldReturnRelativePath()
+        {
+            var absolutePath = RouteConstant.GetAllRoutes().First();
+            var relativePath = absolutePath.GetRelativePath();
+
+            Assert.Equal(absolutePath[1..], relativePath);
+        }
+    }
+}
