6b9782c2b0ed175eecccf8ca54ce03fb3bda9457
Diego Faria <diego.rdfaria@gmail.com>
2023-09-22 23:03:22 -0400

    Migrate project do server side app

---
+++ MyDeveloperTools.ServerApp/Constants/RouteConstant.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace MyDeveloperTools.ServerApp.Constants
+{
+    public static class RouteConstant
+    {
+        public const string Base64Converter = "/tools/base64-converter";
+        public const string GuidGenerator = "/tools/guid-generator";
+        public const string TemperatureConverter = "/tools/temperature-converter";
+
+        public static string GetRelativePath(this string path) => path.TrimStart('/');
+        public static string GetPathTitle(this string path)
+        {
+            return string.Join(' ', path
+                .Split('/')
+                .Last()
+                .Split('-')
+                .Select(x => x[0].ToString().ToUpper() + x[1..]));
+        }
+
+        public static IEnumerable<string> GetAllRoutes()
+        {
+            return typeof(RouteConstant)
+                .GetFields(BindingFlags.Public | BindingFlags.Static)
+                .Where(x => x.IsLiteral && !x.IsInitOnly && x.FieldType == typeof(string))
+                .Select(x => x.GetValue(null)!.ToString()!)
+                .ToList();
+        }
+    }
+}
---
+++ MyDeveloperTools.ServerApp/Program.cs
@@ -0,0 +1,46 @@
+using MudBlazor;
+using MudBlazor.Services;
+using MyDeveloperTools.Core.Interfaces;
+using MyDeveloperTools.Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopEnd;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 2000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
+builder.Services.AddScoped<IBase64Converter, ToolService>();
+builder.Services.AddScoped<ITemperatureConverter, ToolService>();
+builder.Services.AddScoped<IClipboardService, ClipboardService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
